1290f063c5984be793f7c78c273c947d00373d51
Eric Wurtzbacher <ericwurtzb@gmail.com>
2019-12-02 23:01:43 -0700

    added username/password to DAL

--- NorthwesternLabs/Controllers/WorkOrdersController.cs
+++ NorthwesternLabs/Controllers/WorkOrdersController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Server Name: northwest2-16.database.windows.net
+//Use SQL Authentication
+//Username: NorthwestLabsAdmin2019
+//Password: IntExAdmin2-16
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
