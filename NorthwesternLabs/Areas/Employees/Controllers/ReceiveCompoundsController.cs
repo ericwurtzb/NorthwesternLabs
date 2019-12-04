e3813ffb59cf10b0639dc631cf3c3acc0300e280
Aaron Hayden <aaronjhayden@gmail.com>
2019-12-04 15:18:53 -0700

    most of the functionality of the demo

--- NorthwesternLabs/Areas/Employees/Controllers/ReceiveCompoundsController.cs
+++ NorthwesternLabs/Areas/Employees/Controllers/ReceiveCompoundsController.cs
@@ -2,7 +2,9 @@
 using NorthwesternLabs.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -94,6 +96,83 @@ namespace NorthwesternLabs.Areas.Employees.Controllers
                 return RedirectToAction("CompoundList", new { id = passWorkOrder });
             }
 
+            return View(compound);
+        }
+
+        //--------------------------------------------------------------------
+        // GET: Employees/Compounds/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compound compound = db.Compounds.Find(id);
+            if (compound == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compound);
+        }
+
+        // POST: Employees/Compounds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CompoundLT,WorkOrderID,CompoundName,DateArrived,ReceivedBy,Weight,Mass,MTD")] Compound compound)
+        {
+            if (ModelState.IsValid)
+            {
+                compound.WorkOrderID = newCurrentWorkOrder;
+                db.Entry(compound).State = EntityState.Modified;
+                db.SaveChanges();
+                int passWorkOrder = compound.WorkOrderID;
+                return RedirectToAction("CompoundList", new { id = passWorkOrder });
+            }
+            return View(compound);
+        }
+
+        // GET: Employees/Compounds/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compound compound = db.Compounds.Find(id);
+            if (compound == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compound);
+        }
+
+        // POST: Employees/Compounds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Compound compound = db.Compounds.Find(id);
+            db.Compounds.Remove(compound);
+            db.SaveChanges();
+            compound.WorkOrderID = newCurrentWorkOrder;
+            int passWorkOrder = compound.WorkOrderID;
+            return RedirectToAction("CompoundList", new { id = passWorkOrder });
+        }
+
+        // GET: Employees/Compounds/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compound compound = db.Compounds.Find(id);
+            if (compound == null)
+            {
+                return HttpNotFound();
+            }
             return View(compound);
         }
 
