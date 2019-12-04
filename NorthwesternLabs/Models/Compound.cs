846e91023d24ba9f8181e3560cf35b9afb143471
Katie Bankhead <bankhkat@byu.edu>
2019-12-04 14:21:00 -0700

    ReceiveCompounds styling

--- NorthwesternLabs/Models/Compound.cs
+++ NorthwesternLabs/Models/Compound.cs
@@ -2,6 +2,7 @@ namespace NorthwesternLabs.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -10,21 +11,27 @@ namespace NorthwesternLabs.Models
     public partial class Compound
     {
         [Key]
+        [DisplayName("Compound LT Number")]
         public int CompoundLT { get; set; }
 
+        [DisplayName("Work Order ID")]
         public int WorkOrderID { get; set; }
 
+        [DisplayName("Compound Name")]
         [StringLength(50)]
         public string CompoundName { get; set; }
 
+        [DisplayName("Date Arrived")]
         public DateTime? DateArrived { get; set; }
 
+        [DisplayName("Date Received By")]
         public int? ReceivedBy { get; set; }
 
         public double? Weight { get; set; }
 
         public double? Mass { get; set; }
 
+        [DisplayName("Maximum Tolerated Dose (MTD)")]
         public double? MTD { get; set; }
     }
 }
