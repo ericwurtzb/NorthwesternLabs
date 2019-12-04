2dee7a5bac616ca7d167f146231c39124bdb6c5a
Aaron Hayden <aaronjhayden@gmail.com>
2019-12-03 17:34:05 -0700

    push

--- NorthwesternLabs/Areas/Employees/Controllers/HomeController.cs
+++ NorthwesternLabs/Areas/Employees/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NorthwesternLabs.Models;
 
 namespace NorthwesternLabs.Areas.Employees.Controllers
 {
