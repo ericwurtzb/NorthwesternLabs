10eaabd232a8c62e1e3a7e10c273f24eb5532483
Eric Wurtzbacher <ericwurtzb@gmail.com>
2019-12-03 17:26:34 -0700

    added Employees area

---
+++ NorthwesternLabs/Areas/Employees/Controllers/CompoundsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthwesternLabs.DAL;
+using NorthwesternLabs.Models;
+
+namespace NorthwesternLabs.Areas.Employees.Controllers
+{
+    public class CompoundsController : Controller
+    {
+        private NorthwesternLabsContext db = new NorthwesternLabsContext();
+
+        // GET: Employees/Compounds
+        public ActionResult Index()
+        {
+            return View(db.Compounds.ToList());
+        }
+
+        // GET: Employees/Compounds/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compound compound = db.Compounds.Find(id);
+            if (compound == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compound);
+        }
+
+        // GET: Employees/Compounds/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Compounds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CompoundLT,CompoundName,DateArrived,ReceivedBy,Weight,Mass,ConfirmationDateTime,MTD")] Compound compound)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Compounds.Add(compound);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(compound);
+        }
+
+        // GET: Employees/Compounds/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compound compound = db.Compounds.Find(id);
+            if (compound == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compound);
+        }
+
+        // POST: Employees/Compounds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CompoundLT,CompoundName,DateArrived,ReceivedBy,Weight,Mass,ConfirmationDateTime,MTD")] Compound compound)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(compound).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(compound);
+        }
+
+        // GET: Employees/Compounds/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compound compound = db.Compounds.Find(id);
+            if (compound == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compound);
+        }
+
+        // POST: Employees/Compounds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Compound compound = db.Compounds.Find(id);
+            db.Compounds.Remove(compound);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
