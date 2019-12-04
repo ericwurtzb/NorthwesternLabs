3ec367536ef28dd76768c24bf62a2f656243c7b3
Eric Wurtzbacher <ericwurtzb@gmail.com>
2019-12-03 17:12:59 -0700

    testieaj

---
+++ NorthwesternLabs/Areas/Employees/Controllers/testeriiiiiiiinoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwesternLabs.Areas.Employees.Controllers
+{
+    public class testeriiiiiiiinoController : Controller
+    {
+        // GET: Employees/testeriiiiiiiino
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
