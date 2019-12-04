c9b836e4a4eb4a7a5efc135ad40aedc4d8434fc8
Eric Wurtzbacher <ericwurtzb@gmail.com>
2019-12-04 11:28:55 -0700

    --

---
+++ NorthwesternLabs/Areas/Customers/Controllers/CustHomeController.cs
@@ -0,0 +1,49 @@
+﻿using NorthwesternLabs.DAL;
+using NorthwesternLabs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace NorthwesternLabs.Areas.Customers.Controllers
+{
+
+    public class CustHomeController : Controller
+    {
+        private NorthwesternLabsContext db = new NorthwesternLabsContext();
+        // GET: Employees/Home
+        [Authorize(Roles = "Customer")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        // GET: Employees/Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Username,Password,CustomerID,EmployeeID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+    }
+}
